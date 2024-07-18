0eff457604b0c26a29d4b827caecb95b0d0435e9
Glynn Bird <glynn.bird@gmail.com>
2024-07-18 15:54:26 +0100

    0.2.0

--- bin/couchgenerator.bin.js
+++ bin/couchgenerator.bin.js
@@ -1,29 +1,60 @@
 #!/usr/bin/env node
 
-const argv = require('yargs')
-  .option('url', {
-    alias: 'u',
-    describe: 'the URL of the CouchDB instance',
-    default: process.env.COUCH_URL ? process.env.COUCH_URL : undefined,
-    demandOption: !process.env.COUCH_URL
-  })
-  .option('database', {
-    alias: ['db', 'd'],
-    describe: 'the CouchDB database name',
-    default: process.env.COUCH_DATABASE ? process.env.COUCH_DATABASE : undefined,
-    demandOption: !process.env.COUCH_DATABASE
-  })
-  .option('template', {
-    alias: 't',
-    describe: 'the path to the document template',
-    default: process.env.COUCH_DOC_TEMPLATE ? process.env.COUCH_DOC_TEMPLATE : null,
-    demandOption: false
-  })
-  .argv
+const syntax = 
+`Syntax:
+--url/-u           (COUCH_URL)           CouchDB URL                        (required)
+--database/--db/-d (COUCH_DATABASE)      CouchDB Datbase name               (required)
+--template/-t      (COUCH_DOC_TEMPLATE)  The path to the document template  (default: ./templates/user.txt )
+`
+const url = process.env.COUCH_URL || 'http://localhost:5984'
+const db = process.env.COUCH_DATABASE
+const template = process.env.COUCH_DOC_TEMPALTE
+const { parseArgs } = require('node:util')
+const argv = process.argv.slice(2)
+const options = {
+  url: {
+    type: 'string',
+    short: 'u',
+    default: url
+  },
+  database: {
+    type: 'string',
+    short: 'd',
+    default: db
+  },
+  db: {
+    type: 'string',
+    default: db
+  },
+  template: {
+    type: 'string',
+    short: 't'
+  },
+  help: {
+    type: 'boolean',
+    short: 'h',
+    default: false
+  }
+}
+
+// parse command-line options
+const { values } = parseArgs({ argv, options })
+
+// help mode
+if (values.help) {
+  console.log(syntax)
+  process.exit(0)
+}
+
+// db is an alias of database
+if (values.db) {
+  values.database = values.db
+  delete values.db
+}
 
 const couchgenerator = require('../index.js')
 couchgenerator({
-  url: argv.url,
-  db: argv.database,
-  template: argv.template
+  url: values.url,
+  db: values.database,
+  template: values.template
 })
